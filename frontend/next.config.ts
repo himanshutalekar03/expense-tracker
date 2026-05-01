4c8d95daf34f5c6cc31674c1702d5f50fc901940
Himanshu Talekar <164555329+himanshutalekar03@users.noreply.github.com>
2026-05-01 17:42:46 +0530

    Remove devIndicators from Next.js configuration
    Removed devIndicators configuration from Next.js config.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    buildActivityPosition: 'bottom-right',
-  },
+  
+  
 };
 
 export default nextConfig;
